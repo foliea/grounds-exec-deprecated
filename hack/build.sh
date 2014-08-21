997388b339bbf572fd03e0fcfb9654e9b9726c66
Adrien Folie <folie.adrien@gmail.com>
2014-08-21 17:47:54 +0200

    Update build.sh

--- hack/build.sh
+++ hack/build.sh
@@ -6,34 +6,34 @@ GIT_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null)
 
 # Set default docker repository if none exist in env
 if [ -z $REPOSITORY ]; then
-  REPOSITORY="grounds"
+	REPOSITORY="grounds"
 fi
 
 # Set default groundsock image if none exist in env
 if [ -z $SERVER_IMAGE ]; then
-  SERVER_IMAGE="$REPOSITORY/groundsock:$GIT_BRANCH"
+	SERVER_IMAGE="$REPOSITORY/groundsock:$GIT_BRANCH"
 fi
 
 # Set default web image if none exist in env
 if [ -z $WEB_IMAGE ]; then
-  WEB_IMAGE="$REPOSITORY/web:$GIT_BRANCH"
+	WEB_IMAGE="$REPOSITORY/web:$GIT_BRANCH"
 fi
 
 server() {
-  docker build -t $(SERVER_IMAGE) .
+	docker build -t $(SERVER_IMAGE) .
 }
 
 web() {
-  docker build -t $(WEB_IMAGE) web
+	docker build -t $(WEB_IMAGE) web
 }
 
 build() {
-  # If first parameter from CLI is missing or empty
-  if [ -z $1 ]; then
-    echo "usage: [server|web]"
-    return
-  fi
-  eval $1
+	# If first parameter from CLI is missing or empty
+	if [ -z $1 ]; then
+		echo "usage: [server|web]"
+		return
+	fi
+	eval $1
 }
 
 build $1
