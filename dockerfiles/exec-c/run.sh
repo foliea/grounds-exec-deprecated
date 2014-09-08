2424ff2332ab2dc1473c2e617bb4fda3bc309bd3
Adrien Folie <folie.adrien@gmail.com>
2014-09-08 10:06:29 +0000

    Revert "still trying to prevent fork bomb"
    This reverts commit fc6ef53131b6440ee92d8b3906621dcfccdbe6af.

--- dockerfiles/exec-c/run.sh
+++ dockerfiles/exec-c/run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 useradd $HOSTNAME
-
+su $HOSTNAME
 ulimit -p 15
 
 echo "$1" > prog.c
@@ -9,5 +9,5 @@ gcc -o prog prog.c
 
 if [ -f "prog" ]
 then
-  sudo -u $HOSTNAME ./prog
+  ./prog
 fi
