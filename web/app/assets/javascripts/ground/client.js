014dcbf7ab06e25f033db6617642e814d24f4e5b
Adrien Folie <folie.adrien@gmail.com>
2014-09-13 01:07:37 +0200

    private object

--- web/app/assets/javascripts/ground/client.js
+++ web/app/assets/javascripts/ground/client.js
@@ -1,43 +1,43 @@
 function Client(endpoint) {
-    this.socket = null;
-    this.connected = false;
+    this._socket = null;
+    this._connected = false;
  
     this.connect(endpoint);
     this.reload();
 }
 
 Client.prototype.reload = function() {
-    this.console = new Console();
+    this._console = new Console();
 };
 
 Client.prototype.connect = function(endpoint) {
     if (endpoint === null) return;
   
-    this.socket = io.connect(endpoint);
+    this._socket = io.connect(endpoint);
     this.bindEvents();
 };
 
 Client.prototype.send = function(event, data) {
-    if (this.connected === false) return;
+    if (this._connected === false) return;
 
-    this.console.startWaiting();
+    this._console.startWaiting();
      
     var request = JSON.stringify(data);
   
-    this.socket.emit(event, request);
+    this._socket.emit(event, request);
 };
 
 Client.prototype.bindEvents = function() {
     var that = this;
-    this.socket.on('run', function(data) {
+    this._socket.on('run', function(data) {
         var response = JSON.parse(data);
-        that.console.write(response.stream, response.chunk);
+        that._console.write(response.stream, response.chunk);
     });
-    this.socket.on('connect', function(data) {
-        that.connected = true;
+    this._socket.on('connect', function(data) {
+        that._connected = true;
     });
-    this.socket.on('connect_error', function() {
-        that.connected = false;
-        that.console.error();
+    this._socket.on('connect_error', function() {
+        that._connected = false;
+        that._console.error();
     });
 };
