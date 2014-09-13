aee1ed3e189bed0524f4bfa2f2b18d8618f3f9d9
Adrien Folie <folie.adrien@gmail.com>
2014-09-14 01:51:38 +0200

    disconnect websocket properly when switching to another page

--- web/app/assets/javascripts/ground/client.js
+++ web/app/assets/javascripts/ground/client.js
@@ -1,29 +1,34 @@
 function Client(endpoint) {
-    this._socket = null;
-    this._connected = false;
- 
-    this.connect(endpoint);
-    this.reload();
-}
-
-Client.prototype.reload = function() {
     this._console = new Console();
-};
+    this._socket = null;
+
+    this.connect(endpoint);
+}
 
 Client.prototype.connect = function(endpoint) {
     if (endpoint === null) return;
-  
-    this._socket = io.connect(endpoint);
+
+    this._socket = io.connect(endpoint, {'forceNew':true });
     this.bindEvents();
 };
 
+Client.prototype.disconnect = function() {
+    if (this.connected() === false) return;
+
+    this._socket.io.disconnect();
+};
+
+Client.prototype.connected = function() {
+    return this._socket !== null && this._socket.connected;
+};
+
 Client.prototype.send = function(event, data) {
-    if (this._connected === false) return;
+    if (this.connected() === false) return;
 
     this._console.startWaiting();
-     
+
     var request = JSON.stringify(data);
-  
+
     this._socket.emit(event, request);
 };
 
@@ -34,11 +39,9 @@ Client.prototype.bindEvents = function() {
         that._console.write(response.stream, response.chunk);
     });
     this._socket.on('connect', function(data) {
-        that._connected = true;
         that._console.clean();
     });
     this._socket.on('connect_error', function() {
-        that._connected = false;
         that._console.error();
     });
 };
